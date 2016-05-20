dcee606ef25cda6b5649b2630b77cde816818e14
Don J. Rude <djr7m@ccri.com>
2016-05-20 14:58:00 -0400

    fix path

--- bin/config.sh
+++ bin/config.sh
@@ -60,7 +60,7 @@ function set_env_vars {
 
   export SPARK_HOME="$CLOUD_HOME/spark-${pkg_spark_ver}-bin-hadoop${pkg_spark_hadoop_ver}"
   
-  export PATH=$ZOOKEEPER_HOME/bin:$KAFKA_HOME/bin:$ACCUMULO_HOME/bin:$HADOOP_HOME/sbin:$HADOOP_HOME/bin:$PATH:$SPARK_HOME/bin
+  export PATH=$ZOOKEEPER_HOME/bin:$KAFKA_HOME/bin:$ACCUMULO_HOME/bin:$HADOOP_HOME/sbin:$HADOOP_HOME/bin:$SPARK_HOME/bin:$PATH
 }
 
 if [[ -z "$JAVA_HOME" ]];then
